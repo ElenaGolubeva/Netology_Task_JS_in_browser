a727edcaa940c41cf366bdd5caff69e94b6f5208
ElenaGolubeva <elenagolubeva2003@mail.ru>
2024-11-11 14:46:53 +0300

    Add alternating size changes when clicked

--- cookie-clicker/task.js
+++ cookie-clicker/task.js
@@ -2,4 +2,11 @@ img = document.getElementById("cookie")
 counter = document.getElementById("clicker__counter")
 img.onclick = () => {
     counter.textContent = Number(counter.textContent) + 1
+    if (Number(counter.textContent % 2 == 1 )) {
+        img.width += 15
+        img.height += 15
+    } else {
+        img.width -= 15
+        img.height -= 15
+    }
 }
